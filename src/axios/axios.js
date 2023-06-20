cb9b9b21ebe4033aa208bc0296dd34a134c12fdc
novakoriss <121388611+VadymHvas@users.noreply.github.com>
2023-06-20 23:53:30 +0300

    Update axios.js

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://note-mern-server.vercel.app",
+    baseURL: "https://note-mern-server.vercel.app/",
 });
 
 instance.interceptors.request.use(config => {
