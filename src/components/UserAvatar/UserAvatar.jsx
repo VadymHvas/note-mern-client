6352be455dd86401150ca12a8f9f90fcb9f826c3
novakoriss <121388611+VadymHvas@users.noreply.github.com>
2023-06-22 18:36:49 +0300

    Update UserAvatar.jsx

--- src/components/UserAvatar/UserAvatar.jsx
+++ src/components/UserAvatar/UserAvatar.jsx
@@ -5,7 +5,7 @@ const UserAvatar = ({user, className}) => {
     return (
         <div className={`user-avatar ${className}`}>
             {user.avatar && (
-                <img src={`https://note-mern-server.vercel.app/userAvatars/${user.avatar}`} alt="errrr" />
+                <img src={`https://note-mern-server.vercel.app/${user.avatar}`} alt="errrr" />
             )}
         </div>
     );
